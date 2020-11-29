eca5f0fa595ddffe01bd766bfbc6275cb17aa0dd
olomskaya sonya <olomskayasonya@olomskayas-MBP.cable.rcn.com>
2020-11-28 23:26:38 -0500

    motion added

---
+++ motion/script.js
@@ -0,0 +1,127 @@
+$(document).ready(function(){
+
+$("#one").mouseover(function(){
+    $("#quote1").css("opacity", 1);
+     $("#2020").css("opacity", 0);
+})
+
+$("#one").mouseleave(function(){
+    $("#quote1").css("opacity", 0);
+     $("#2020").css("opacity", 1);
+})
+
+
+$("#two").mouseover(function(){
+    $("#quote2").css("opacity", 1);
+     $("#2020").css("opacity", 0);
+})
+
+$("#two").mouseleave(function(){
+    $("#quote2").css("opacity", 0);
+     $("#2020").css("opacity", 1);
+})
+
+$("#three").mouseover(function(){
+    $("#quote3").css("opacity", 1);
+     $("#2020").css("opacity", 0);
+})
+
+$("#three").mouseleave(function(){
+    $("#quote3").css("opacity", 0);
+     $("#2020").css("opacity", 1);
+})
+
+$("#four").mouseover(function(){
+    $("#quote4").css("opacity", 1);
+     $("#2020").css("opacity", 0);
+})
+
+$("#four").mouseleave(function(){
+    $("#quote4").css("opacity", 0);
+     $("#2020").css("opacity", 1);
+})
+
+$("#five").mouseover(function(){
+    $("#quote5").css("opacity", 1);
+     $("#2020").css("opacity", 0);
+})
+
+$("#five").mouseleave(function(){
+    $("#quote5").css("opacity", 0);
+     $("#2020").css("opacity", 1);
+})
+
+$("#six").mouseover(function(){
+    $("#quote6").css("opacity", 1);
+     $("#2020").css("opacity", 0);
+})
+
+$("#six").mouseleave(function(){
+    $("#quote6").css("opacity", 0);
+     $("#2020").css("opacity", 1);
+})
+
+
+$("#seven").mouseover(function(){
+    $("#quote7").css("opacity", 1);
+     $("#2020").css("opacity", 0);
+})
+
+$("#seven").mouseleave(function(){
+    $("#quote7").css("opacity", 0);
+     $("#2020").css("opacity", 1);
+})
+
+$("#eight").mouseover(function(){
+    $("#quote8").css("opacity", 1);
+     $("#2020").css("opacity", 0);
+})
+
+$("#eight").mouseleave(function(){
+    $("#quote8").css("opacity", 0);
+     $("#2020").css("opacity", 1);
+})
+
+$("#nine").mouseover(function(){
+    $("#quote9").css("opacity", 1);
+     $("#2020").css("opacity", 0);
+})
+
+$("#nine").mouseleave(function(){
+    $("#quote9").css("opacity", 0);
+     $("#2020").css("opacity", 1);
+})
+
+$("#ten").mouseover(function(){
+    $("#quote10").css("opacity", 1);
+     $("#2020").css("opacity", 0);
+})
+
+$("#ten").mouseleave(function(){
+    $("#quote10").css("opacity", 0);
+     $("#2020").css("opacity", 1);
+})
+
+
+$("#eleven").mouseover(function(){
+    $("#quote11").css("opacity", 1);
+     $("#2020").css("opacity", 0);
+})
+
+$("#eleven").mouseleave(function(){
+    $("#quote11").css("opacity", 0);
+     $("#2020").css("opacity", 1);
+})
+
+$("#twelve").mouseover(function(){
+    $("#quote12").css("opacity", 1);
+     $("#2020").css("opacity", 0);
+})
+
+$("#twelve").mouseleave(function(){
+    $("#quote12").css("opacity", 0);
+     $("#2020").css("opacity", 1);
+})
+
+
+});
