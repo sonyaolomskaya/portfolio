3e76751417f2571ed90d89c3353bb6f682c410f3
sonyaolomskaya <60186172+sonyaolomskaya@users.noreply.github.com>
2020-12-15 18:47:51 -0500

    motion final

---
+++ motion/resjava.js
@@ -0,0 +1,285 @@
+
+$(document).ready(function(){
+
+$("#one").mouseover(function(){
+    $("#quote1").css("opacity", 1);
+})
+
+$("#one").mouseleave(function(){
+    $("#quote1").css("opacity", 0);
+})
+
+$("#two").mouseover(function(){
+    $("#quote2").css("opacity", 1);
+})
+
+$("#two").mouseleave(function(){
+    $("#quote2").css("opacity", 0);
+})
+
+$("#three").mouseover(function(){
+    $("#quote3").css("opacity", 1);
+})
+
+$("#three").mouseleave(function(){
+    $("#quote3").css("opacity", 0);
+})
+
+$("#four").mouseover(function(){
+    $("#quote4").css("opacity", 1);
+})
+
+$("#four").mouseleave(function(){
+    $("#quote4").css("opacity", 0);
+})
+
+$("#five").mouseover(function(){
+    $("#quote5").css("opacity", 1);
+})
+
+$("#five").mouseleave(function(){
+    $("#quote5").css("opacity", 0);
+})
+
+$("#six").mouseover(function(){
+    $("#quote6").css("opacity", 1);
+})
+
+$("#six").mouseleave(function(){
+    $("#quote6").css("opacity", 0);
+})
+
+
+$("#seven").mouseover(function(){
+    $("#quote7").css("opacity", 1);
+})
+
+$("#seven").mouseleave(function(){
+    $("#quote7").css("opacity", 0);
+})
+
+$("#eight").mouseover(function(){
+    $("#quote8").css("opacity", 1);
+})
+
+$("#eight").mouseleave(function(){
+    $("#quote8").css("opacity", 0);
+})
+
+$("#nine").mouseover(function(){
+    $("#quote9").css("opacity", 1);
+})
+
+$("#nine").mouseleave(function(){
+    $("#quote9").css("opacity", 0);
+})
+
+$("#ten").mouseover(function(){
+    $("#quote10").css("opacity", 1);
+})
+
+$("#ten").mouseleave(function(){
+    $("#quote10").css("opacity", 0);
+})
+
+
+$("#eleven").mouseover(function(){
+    $("#quote11").css("opacity", 1);
+})
+
+$("#eleven").mouseleave(function(){
+    $("#quote11").css("opacity", 0);
+})
+
+$("#twelve").mouseover(function(){
+    $("#quote12").css("opacity", 1);
+})
+
+$("#twelve").mouseleave(function(){
+    $("#quote12").css("opacity", 0);
+})
+
+
+document.getElementById('videoone').onclick = function () { 
+    document.getElementById('two').pause();
+    document.getElementById('three').pause();
+    document.getElementById('four').pause();
+    document.getElementById('five').pause();
+    document.getElementById('six').pause();
+    document.getElementById('seven').pause();
+    document.getElementById('eight').pause();
+    document.getElementById('nine').pause();
+    document.getElementById('ten').pause();
+    document.getElementById('eleven').pause();
+    document.getElementById('twelve').pause();
+
+};
+
+document.getElementById('videotwo').onclick = function () { 
+    document.getElementById('one').pause();
+    document.getElementById('three').pause();
+    document.getElementById('four').pause();
+    document.getElementById('five').pause();
+    document.getElementById('six').pause();
+    document.getElementById('seven').pause();
+    document.getElementById('eight').pause();
+    document.getElementById('nine').pause();
+    document.getElementById('ten').pause();
+    document.getElementById('eleven').pause();
+    document.getElementById('twelve').pause();
+
+};
+
+document.getElementById('videothree').onclick = function () { 
+    document.getElementById('one').pause();
+    document.getElementById('two').pause();
+    document.getElementById('four').pause();
+    document.getElementById('five').pause();
+    document.getElementById('six').pause();
+    document.getElementById('seven').pause();
+    document.getElementById('eight').pause();
+    document.getElementById('nine').pause();
+    document.getElementById('ten').pause();
+    document.getElementById('eleven').pause();
+    document.getElementById('twelve').pause();
+
+};
+
+document.getElementById('videofour').onclick = function () { 
+    document.getElementById('one').pause();
+    document.getElementById('two').pause();
+    document.getElementById('three').pause();
+    document.getElementById('five').pause();
+    document.getElementById('six').pause();
+    document.getElementById('seven').pause();
+    document.getElementById('eight').pause();
+    document.getElementById('nine').pause();
+    document.getElementById('ten').pause();
+    document.getElementById('eleven').pause();
+    document.getElementById('twelve').pause();
+
+};
+
+
+document.getElementById('videofive').onclick = function () { 
+    document.getElementById('one').pause();
+    document.getElementById('two').pause();
+    document.getElementById('three').pause();
+    document.getElementById('four').pause();
+    document.getElementById('six').pause();
+    document.getElementById('seven').pause();
+    document.getElementById('eight').pause();
+    document.getElementById('nine').pause();
+    document.getElementById('ten').pause();
+    document.getElementById('eleven').pause();
+    document.getElementById('twelve').pause();
+
+};
+
+
+document.getElementById('videosix').onclick = function () { 
+    document.getElementById('one').pause();
+    document.getElementById('two').pause();
+    document.getElementById('three').pause();
+    document.getElementById('four').pause();
+    document.getElementById('five').pause();
+    document.getElementById('seven').pause();
+    document.getElementById('eight').pause();
+    document.getElementById('nine').pause();
+    document.getElementById('ten').pause();
+    document.getElementById('eleven').pause();
+    document.getElementById('twelve').pause();
+
+};
+
+document.getElementById('videoseven').onclick = function () { 
+    document.getElementById('one').pause();
+    document.getElementById('two').pause();
+    document.getElementById('three').pause();
+    document.getElementById('four').pause();
+    document.getElementById('six').pause();
+    document.getElementById('five').pause();
+    document.getElementById('eight').pause();
+    document.getElementById('nine').pause();
+    document.getElementById('ten').pause();
+    document.getElementById('eleven').pause();
+    document.getElementById('twelve').pause();
+
+};
+
+document.getElementById('videoeight').onclick = function () { 
+    document.getElementById('one').pause();
+    document.getElementById('two').pause();
+    document.getElementById('three').pause();
+    document.getElementById('four').pause();
+    document.getElementById('six').pause();
+    document.getElementById('five').pause();
+    document.getElementById('seven').pause();
+    document.getElementById('nine').pause();
+    document.getElementById('ten').pause();
+    document.getElementById('eleven').pause();
+    document.getElementById('twelve').pause();
+
+};
+
+document.getElementById('videonine').onclick = function () { 
+    document.getElementById('one').pause();
+    document.getElementById('two').pause();
+    document.getElementById('three').pause();
+    document.getElementById('four').pause();
+    document.getElementById('six').pause();
+    document.getElementById('five').pause();
+    document.getElementById('seven').pause();
+    document.getElementById('eight').pause();
+    document.getElementById('ten').pause();
+    document.getElementById('eleven').pause();
+    document.getElementById('twelve').pause();
+
+};
+
+document.getElementById('videoten').onclick = function () { 
+    document.getElementById('one').pause();
+    document.getElementById('two').pause();
+    document.getElementById('three').pause();
+    document.getElementById('four').pause();
+    document.getElementById('six').pause();
+    document.getElementById('five').pause();
+    document.getElementById('seven').pause();
+    document.getElementById('eight').pause();
+    document.getElementById('nine').pause();
+    document.getElementById('eleven').pause();
+    document.getElementById('twelve').pause();
+
+};
+
+document.getElementById('videoeleven').onclick = function () { 
+    document.getElementById('one').pause();
+    document.getElementById('two').pause();
+    document.getElementById('three').pause();
+    document.getElementById('four').pause();
+    document.getElementById('six').pause();
+    document.getElementById('five').pause();
+    document.getElementById('seven').pause();
+    document.getElementById('eight').pause();
+    document.getElementById('nine').pause();
+    document.getElementById('ten').pause();
+    document.getElementById('twelve').pause();
+
+};
+
+document.getElementById('videotwelve').onclick = function () { 
+    document.getElementById('one').pause();
+    document.getElementById('two').pause();
+    document.getElementById('three').pause();
+    document.getElementById('four').pause();
+    document.getElementById('six').pause();
+    document.getElementById('five').pause();
+    document.getElementById('seven').pause();
+    document.getElementById('eight').pause();
+    document.getElementById('nine').pause();
+    document.getElementById('ten').pause();
+    document.getElementById('eleven').pause();
+
+};
+
+});
