d1cbf2d26516adf2e1eb9a983f73a16787d63594
sonyaolomskaya <60186172+sonyaolomskaya@users.noreply.github.com>
2021-03-29 20:56:18 +0800

    test

---
+++ smithsonian/script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+
+$(".text1").mouseover(function(){
+    $(".textone").css("opacity", 1);
+})
+
+$(".text1").mouseleave(function(){
+    $(".textone").css("opacity", 0);
+})
+
+});
